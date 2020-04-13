457883813ef54379e8741faccf28616ca597aa3d
texasnick <nickryan@gmail.com>
2020-04-13 14:20:39 -0500

    v0.4 updated all var references and kub block

--- aks.tf
+++ aks.tf
@@ -1,6 +1,6 @@
 resource "azurerm_resource_group" "k8s" {
-    name     = "${var.resource_group_name}"
-    location = "${var.location}"
+    name     = var.resource_group_name
+    location = var.location
 }
 
 resource "random_id" "log_analytics_workspace_name_suffix" {
@@ -9,18 +9,18 @@ resource "random_id" "log_analytics_workspace_name_suffix" {
 
 resource "azurerm_log_analytics_workspace" "test" {
     # The WorkSpace name has to be unique across the whole of azure, not just the current subscription/tenant.
-    name                = "${var.log_analytics_workspace_name}-${random_id.log_analytics_workspace_name_suffix.dec}"
-    location            = "${var.log_analytics_workspace_location}"
-    resource_group_name = "${azurerm_resource_group.k8s.name}"
-    sku                 = "${var.log_analytics_workspace_sku}"
+    name                = var.log_analytics_workspace_name-random_id.log_analytics_workspace_name_suffix.dec
+    location            = var.log_analytics_workspace_location
+    resource_group_name = azurerm_resource_group.k8s.name
+    sku                 = var.log_analytics_workspace_sku
 }
 
 resource "azurerm_log_analytics_solution" "test" {
     solution_name         = "ContainerInsights"
-    location              = "${azurerm_log_analytics_workspace.test.location}"
-    resource_group_name   = "${azurerm_resource_group.k8s.name}"
-    workspace_resource_id = "${azurerm_log_analytics_workspace.test.id}"
-    workspace_name        = "${azurerm_log_analytics_workspace.test.name}"
+    location              = azurerm_log_analytics_workspace.test.location
+    resource_group_name   = azurerm_resource_group.k8s.name
+    workspace_resource_id = azurerm_log_analytics_workspace.test.id
+    workspace_name        = azurerm_log_analytics_workspace.test.name
 
     plan {
         publisher = "Microsoft"
@@ -29,36 +29,42 @@ resource "azurerm_log_analytics_solution" "test" {
 }
 
 resource "azurerm_kubernetes_cluster" "k8s" {
-    name                = "${var.cluster_name}"
-    location            = "${azurerm_resource_group.k8s.location}"
-    resource_group_name = "${azurerm_resource_group.k8s.name}"
-    dns_prefix          = "${var.dns_prefix}"
+    name                = var.cluster_name
+    location            = azurerm_resource_group.k8s.location
+    resource_group_name = azurerm_resource_group.k8s.name
+    dns_prefix          = var.dns_prefix
 
     linux_profile {
         admin_username = "ubuntu"
 
         ssh_key {
-            key_data = "${file("${var.ssh_public_key}")}"
+            key_data = "${file(var.ssh_public_key)}"
         }
     }
 
-    agent_pool_profile {
-        name            = "agentpool"
-        count           = "${var.agent_count}"
-        vm_size         = "Standard_DS1_v2"
-        os_type         = "Linux"
-        os_disk_size_gb = 30
+    #agent_pool_profile {
+    #    name            = "agentpool"
+    #    count           = var.agent_count
+    #    vm_size         = "Standard_DS1_v2"
+    #    os_type         = "Linux"
+    #    os_disk_size_gb = 30
+    #}
+    
+    default_node_pool {
+        name       = "default"
+        node_count = 1
+        vm_size    = "Standard_D2_v2"
     }
 
     service_principal {
-        client_id     = "${var.clientId}"
-        client_secret = "${var.clientSecret}"
+        client_id     = var.clientId
+        client_secret = var.clientSecret
     }
 
     addon_profile {
         oms_agent {
-        enabled                    = true
-        log_analytics_workspace_id = "${azurerm_log_analytics_workspace.test.id}"
+            enabled                    = true
+            log_analytics_workspace_id = azurerm_log_analytics_workspace.test.id
         }
     }
 
