ca0649a9e04e6f6396aa126545afbdae42d70e94
texasnick <nickryan@gmail.com>
2020-04-14 11:57:09 -0500

    v0.4.4.2 name test again

--- aks.tf
+++ aks.tf
@@ -14,7 +14,7 @@ resource "tls_private_key" "lnxkey" {
 
 resource "azurerm_log_analytics_workspace" "test" {
     # The WorkSpace name has to be unique across the whole of azure, not just the current subscription/tenant.
-    name                = var.log_analytics_workspace_name"-"random_id.log_analytics_workspace_name_suffix.dec
+    name                = "${var.log_analytics_workspace_name}-${random_id.log_analytics_workspace_name_suffix.dec}"
     location            = var.log_analytics_workspace_location
     resource_group_name = azurerm_resource_group.k8s.name
     sku                 = var.log_analytics_workspace_sku
