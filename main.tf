8cd9ab3b9f591a9cf18d2b7c15381fbcb16a149e
texasnick <nickryan@gmail.com>
2020-04-17 08:48:01 -0500

    v0.4.4.5 pinning to v1.x didn't work

--- main.tf
+++ main.tf
@@ -3,6 +3,6 @@ provider "azurerm" {
   client_id       = var.clientId
   client_secret   = var.clientSecret
   tenant_id       = var.tenantId
-  version = "~> 1.x"
+  version = "=1.44.0"
   #features {}
 }
